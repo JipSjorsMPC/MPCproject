3a800126d373ea61c4cf454afc5bb30ab3f85a1c
Sjorsvanve <sjorsvanve@users.noreply.github.com>
2021-03-21 16:40:46 +0100

    Update linearizePendulumDynamics.m

--- Matlab/MPCRegulator/linearizePendulumDynamics.m
+++ Matlab/MPCRegulator/linearizePendulumDynamics.m
@@ -13,30 +13,29 @@ function [A,B] = linearizePendulumDynamics(xref,uref)
 
 global Lr mp Lp Rm kt km g Br Bp Jr Jp
 
-syms x1 x2 x3 x4 u
+syms x [4 1], syms u
 
 % Define the manipulator form H(q)qdd+C(qd,q)qd+G(q)=Bu
-H = [Jr+mp*Lr^2+1/4*mp*Lp^2*sin(x2)^2 -1/2*mp*Lr*Lp*cos(x2);...
-    -1/2*mp*Lr*Lp*cos(x2) Jp+1/4*mp*Lp^2];
-C = [1/4*mp*Lp^2*sin(2*x2)*x4+Br 0.5*mp*Lr*Lp*x4*sin(x2);...
--1/8*mp*Lp^2*x3*sin(2*x2) Bp];
-G = [0; -0.5*mp*Lp*g*sin(x2)];
+H = [Jr+mp*Lr^2+1/4*mp*Lp^2*sin(x(2))^2 -1/2*mp*Lr*Lp*cos(x(2));...
+    -1/2*mp*Lr*Lp*cos(x(2)) Jp+1/4*mp*Lp^2];
+C = [1/4*mp*Lp^2*sin(2*x(2))*x4+Br 0.5*mp*Lr*Lp*x(4)*sin(x(2));...
+-1/8*mp*Lp^2*x(3)*sin(2*x(2)) Bp];
+G = [0; -0.5*mp*Lp*g*sin(x(2))];
 Bt = [1; 0];
 
 %Include actuator dynamics
-tau = kt*(u-km*x3)/Rm;
+tau = kt*(u-km*x(3))/Rm;
 
 %Obtain dxdt = f(x,u,t);
-dxdt = [x3; x4;...
-    H\(Bt*tau-C*[x3; x4]-G)];
+dxdt = [x(3); x(4);...
+    H\(Bt*tau-C*[x(3); x(4)]-G)];
 
 %Now linearize dxdt = f(x,u,t) w.r.t x and u;
-dfdx =  simplify(jacobian(dxdt,[x1; x2; x3; x4]));
+dfdx =  simplify(jacobian(dxdt,x));
 dfdu = simplify(jacobian(dxdt,u));
 
 %Get the A = dfdx(xref,uref) and B = dfdu(xref,uref);
-A = subs(dfdx,[x1 x2 x3 x4 u],[xref' uref]); 
-B = subs(dfdu,[x1 x2 x3 x4 u],[xref' uref]);
+A = subs(dfdx,[x.' u],[xref' uref]); 
+B = subs(dfdu,[x.' u],[xref' uref]);
 
 end
-
